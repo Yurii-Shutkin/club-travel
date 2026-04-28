bd2ec0e0c4b05f2a03e30d5365983cbfb827d88b
Vadim-48 <rvadim4835@gmail.com>
2026-04-28 22:19:44 +0300

    Updates

---
+++ assets/js/company-BLv_dSH_.js
@@ -0,0 +1,1 @@
+import{t as e}from"./render-news-cards-frweQqh8.js";document.addEventListener("DOMContentLoaded",async()=>{await e(".news")});
--- assets/js/company-BMbpeUIB.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./render-news-cards-DQOkozUD.js";document.addEventListener("DOMContentLoaded",async()=>{await e(".news")});
