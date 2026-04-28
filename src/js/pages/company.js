1c102eb9ad7ec70255441d760a6b15046bd50c90
MaksymSelezen <foqney@gmail.com>
2026-04-28 15:48:09 +0300

    add sticky header and background-color

--- src/js/pages/company.js
+++ src/js/pages/company.js
@@ -1,11 +1,9 @@
 import '../burger-menu.js';
 import '../header-account-dropdown.js';
+import '../header-scroll-state.js';
 
 import { renderNewsCards } from '@/js/components/render-news-cards.js';
 
-
 document.addEventListener('DOMContentLoaded', async () => {
-
   await renderNewsCards('.news');
-
 });
