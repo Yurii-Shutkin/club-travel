eb2c82f43367b76a3077e18924dd6da887f7cb5b
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-05-02 05:36:34 +0300

    added form validate script by using just validate plugin

---
+++ src/js/pages/auth.js
@@ -0,0 +1,8 @@
+import '../burger-menu.js';
+import '../header-account-dropdown.js';
+import '../header-scroll-state.js';
+
+import { initValidation } from '../services/formValidation.js';
+
+initValidation('#form');
+
