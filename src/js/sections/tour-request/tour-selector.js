f60ff256e67b9cda893c01bcd70cdea9f8ba9023
Vadim-48 <rvadim4835@gmail.com>
2026-05-02 04:09:30 +0300

    tour-selector, html+scss
    need fix callback-request

---
+++ src/js/sections/tour-request/tour-selector.js
@@ -0,0 +1,6 @@
+
+
+export const initTourSelector = async (root = document) => {
+const tourSelector = document.querySelector('[data-tour-selector]');
+const tabsList = tourSelector.querySelectorAll('[data-tab]');
+};
