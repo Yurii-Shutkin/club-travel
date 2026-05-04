2672dcf87873a836146ccdbf0ea1682014e9413d
Vadim-48 <rvadim4835@gmail.com>
2026-05-04 19:29:33 +0300

    move files

--- src/js/pages/auth.js
+++ src/js/pages/auth.js
@@ -1,6 +1,6 @@
-import '../burger-menu.js';
-import '../header-account-dropdown.js';
-import '../header-scroll-state.js';
+import '../layout/burger-menu.js';
+import '../layout/header-account-dropdown.js';
+import '../layout/header-scroll-state.js';
 
 import { initValidation } from '../services/formValidation.js';
 
