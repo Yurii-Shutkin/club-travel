f17cc402f9c9209b9502c125def349f157d491fe
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-05-05 10:03:56 +0300

    Updates

--- assets/js/company-BB-V_S4p.js
+++
@@ -1,1 +0,0 @@
-import"./header-scroll-state-QTP0zqH0.js";import{t as e}from"./render-news-cards-CO1KInU2.js";document.addEventListener("DOMContentLoaded",async()=>{await e(".news")});
---
+++ assets/js/password-recovery-e3XrCVFh.js
@@ -0,0 +1,1 @@
+import"./header-scroll-state-QTP0zqH0.js";import{t as i}from"./formValidation-CSLp6KKk.js";i("#form");
