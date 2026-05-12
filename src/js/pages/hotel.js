7ff677ad9c0b589e21456ce3e7f63f407366080f
Vadim-48 <rvadim4835@gmail.com>
2026-05-12 22:43:01 +0300

    fix hotel.js & custom-select

--- src/js/pages/hotel.js
+++ src/js/pages/hotel.js
@@ -5,7 +5,7 @@ import '@/js/sections/main/hero-swiper.js';
 
 import { renderHotCards } from '@/js/components/render-hot-cards.js';
 import { initCardsSwiper } from '@/js/sections/main/cards-swiper.js';
-import { handleGoogleCallback } from '@/js/services/user/auth.js';
+// import { handleGoogleCallback } from '@/js/services/user/auth.js';
 import { selectOpen, updateGuestValue } from '@/js/ui/custom-select.js';
 import { initCounter } from '@/js/ui/counter.js';
 import { innitSwiperThumbs } from '@/js/sections/tour-request/swiper-humbs.js';
@@ -22,13 +22,18 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 
   await Promise.all([
-    handleGoogleCallback(),
+    // handleGoogleCallback(),
 
     renderHotCards('.hotel-page__promo-tours',5).then(() =>
       initCardsSwiper('.hotel-page__promo-tours', 3),
     ),
   ]);
 });
+
+import { getHotels } from '@/js/services/api/getHotels.js';
+
+const hotels = await getHotels(50);
+console.log('hotels', hotels);
 
 
 
