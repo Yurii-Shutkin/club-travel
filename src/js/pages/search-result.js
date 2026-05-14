57aa05b6289b2ce53e8d9003ae9ef2321e07ec15
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-05-14 17:58:05 +0300

    inited search-result page

---
+++ src/js/pages/search-result.js
@@ -0,0 +1,6 @@
+import '@/js/layout/burger-menu.js';
+import '@/js/layout/header-account-dropdown.js';
+import '@/js/layout/header-scroll-state.js';
+import '@/js/sections/main/hero-swiper.js';
+import '@/js/components/tour-search.js';
+
